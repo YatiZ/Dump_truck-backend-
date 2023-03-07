12b742b0f1c92d8eb21b974db3acbbf4077e0f69
YatiZ <zuzu62113@gmail.com>
2023-03-08 02:40:47 +0630

    create_models

--- car_app/models.py
+++ car_app/models.py
@@ -3,7 +3,54 @@ import uuid
 from datetime import datetime
 # Create your models here.
 
+# class CarInfo(models.TextChoices):
+#     FIRST_CAR = "1P",'1P'
+#     SECOND_CAR = "7Q",'7Q'
+#     THIRD_CAR = "5R",'5R'
+
 class CarInfo(models.Model):
-    car_no = models.TextField()
+    car_no = models.TextField(max_length=50)
+    maintenance =  models.TextField(blank=True)
+    # def __str__(self):
+    #     return self.car_no
+
+class CustomerInfo(models.Model):
+    customer_name = models.CharField(max_length=200)
+    created_at = models.DateField()
+    updated_at = models.DateField(auto_now_add=True)
+    deleted_at = models.DateField(auto_now_add=True)
+    phone_no = models.TextField(max_length=50, blank=True)
+    address = models.TextField(max_length=100, blank= True)
+    def __str__(self):
+        return f"{self.customer_name}{self.created_at}"
+
+class Order(models.Model):
+    car_id = models.ForeignKey(CarInfo,blank=True,on_delete=models.CASCADE)
+    customer_id = models.ForeignKey(CustomerInfo,blank=True,on_delete=models.CASCADE)
+    created_at = models.DateField()
+    updated_at = models.DateField(auto_now_add=True)
+    deleted_at = models.DateField(auto_now_add=True)
+    count = models.IntegerField(blank=True)
+    service_fees_per_count = models.TextField(max_length=100,blank=True)
+    debt_amount = models.TextField(max_length=100,blank=True)
+    description = models.TextField(max_length=200, blank=True)
+    paid_amount = models.TextField(max_length=100,blank=True)
+    
+
+class Investment(models.Model):
+    car_id =  models.ForeignKey(CarInfo,blank=True,on_delete=models.CASCADE)
+    created_at = models.DateField()
+    updated_at = models.DateField(auto_now_add=True)
+    deleted_at = models.DateField(auto_now_add=True)
+    petrol = models.TextField(blank=True)
+    driver_fees = models.TextField()
+    extra_cost = models.TextField(blank=True)
+    cost_for_home = models.TextField()
+
+
+    
+
+
+
 
 
--- car_app/views.py
+++ car_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import CarInfo,CustomerInfo,Investment,Order
+from .serializers import CarInfoSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 
 # Create your views here.
+
+def index(request):
+    orders = Order.objects.all()
+    carinfo = CarInfo.objects.all()
+    context = {'carinfo':carinfo,'orders':orders}
+    return render(request,'index.html',context,)
+
+class CarData(APIView):
+    def get(self,request,format = None):
+        carinfo = CarInfo.objects.all()
+        serializer = CarInfoSerializer(carinfo,many=True)
+        return Response(serializer.data)
